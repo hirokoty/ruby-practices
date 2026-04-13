d791bfa4dd7ac2981868586d498029921796dcce
LHeHI7xDTr <hirokotyu789@gmail.com>
2026-04-14 02:59:08 +0900

    コードをシンプルに修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,21 +1,11 @@
-def fizzbuzz(min:, max:, div1:, div2:)
-  errors = []
-  errors << "範囲が不正です" if max < min
-  errors << "0で割ることはできません" if [div1, div2].include?(0)
-  return puts(errors) unless errors.empty?
-
-  both = div1.lcm(div2)
-  range = min..max
-  range.each do |num|
-    case
-    when num % both == 0 then puts "FizzBuzz"
-    when num % div1 == 0 then puts "Fizz"
-    when num % div2 == 0 then puts "Buzz"
-    else  puts "#{num}"
-    end
+(1..20).each do |i|
+  if i % 15 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  else 
+    puts i
   end
 end
-
-fizzbuzz(min: 1, max: 20, div1: 3, div2: 5)
-#引数の数値を変えて実行することも、できるようにしてみました
-
