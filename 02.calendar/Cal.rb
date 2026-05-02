756b5cc9d141a9e364d261380f83f8b20a820e65
LHeHI7xDTr <hirokotyu789@gmail.com>
2026-05-02 19:58:06 +0900

    PRを受けて変数関係を修正

--- 02.calendar/Cal.rb
+++ 02.calendar/Cal.rb
@@ -2,15 +2,18 @@
 require "date"
 require 'optparse'
 
-this_year  = Time.now.year
-this_month = Time.now.month
-params = ARGV.getopts("", "year:#{this_year}", "month:#{this_month}")
-day_end = Date.new(params["year"].to_i, params["month"].to_i, -1).day
-date_1st = Date.new(params["year"].to_i, params["month"].to_i, 1)
+today = Date.today
+params = ARGV.getopts("", "year:#{today.year}", "month:#{today.month}")
 
-puts "#{params["year"]}年   #{params["month"]}月"
+year  = params["year"].to_i
+month = params["month"].to_i
+first_date    = Date.new(year, month, 1)
+days_in_month = Date.new(year, month, -1).day
+
+puts "#{year}年   #{month}月"
 puts "日 月 火 水 木 金 土"
-days = (["  "] * date_1st.wday) + (1..day_end).map { |d| d.to_s.rjust(2) }
+days = Array.new(first_date.wday, "  ") + (1..days_in_month).map { |d| d.to_s.rjust(2) }
 days.each_slice(7) do |week|
   puts week.join(" ")
 end
+
