afb4375ec3f4c6f85893b920a74fc00c1eda5005
hirokoty <hirokotyu789@gmail.com>
2026-05-17 10:27:09 +0900

    定数への変更修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+TOTAL_PINS = 10          # フレームの総ピン数
+
 input = ARGV[0]
-shots = input.split(',').map { |s| s == 'X' ? 10 : s.to_i }
+shots = input.split(',').map { |s| s == 'X' ? TOTAL_PINS : s.to_i }
 
 scores = []
 9.times do
   shot1 = shots.shift
-  frame_score = if shot1 == 10
-                  10 + shots[0] + shots[1]
+  frame_score = if shot1 == TOTAL_PINS
+                  TOTAL_PINS + shots[0] + shots[1]
                 else
                   shot2 = shots.shift
                   frame_total = shot1 + shot2
-                  if frame_total == 10
-                    10 + shots[0]
+                  if frame_total == TOTAL_PINS
+                    TOTAL_PINS + shots[0]
                   else
                     frame_total
                   end
